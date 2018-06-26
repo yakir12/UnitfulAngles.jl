f93ee6d8e254143ad8c80cab2855e5960cc5c1bc
Yakir Luc Gagnon <12.yakir@gmail.com>
2018-06-26 14:27:21 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
-using UnitfulAngles
-using Unitful
-using Base.Test
+using Unitful, UnitfulAngles
+@static if VERSION < v"0.7.0-DEV.2005"
+    using Base.Test
+else
+    using Test
+end
 
 units = (u"doubleTurn", u"turn", u"halfTurn", u"quadrant", u"sextant", u"octant", u"clockPosition", u"hourAngle", u"compassPoint", u"hexacontade", u"brad", u"°", u"grad", u"arcminute", u"arcsecond", u"rad", u"diameterPart")
 quantities = (0.5, 1, 2, 4, 6, 8, 12, 24, 32, 60, 256, 360, 400, 21600, 1296000, 2π, 120π)
